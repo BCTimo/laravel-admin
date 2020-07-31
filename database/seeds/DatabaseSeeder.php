b68c522518cba9d01f2075bf6615a78a7aefecc9
Timo BC <bctimo@gmail.com>
2020-07-31 13:28:12 +0800

    video seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminConfigTableSeeder::class);
         $this->call(VideoTagsTableSeeder::class);
         $this->call(DomainsTableSeeder::class);
+        $this->call(VideofilesTableSeeder::class);
     }
 }
