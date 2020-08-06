d5e6a0a38981477c95313567df7c89f0b0c9a421
Timo BC <bctimo@gmail.com>
2020-08-06 10:22:26 +0800

    base65.html

--- app/Jobs/ProcessM3U8.php
+++ app/Jobs/ProcessM3U8.php
@@ -51,7 +51,6 @@ class ProcessM3U8 implements ShouldQueue
      */
     public function handle()
     {
-
         $MV_path = public_path().'/MV/'.$this->videoId;
         $iv = '3c44008a7e2e5f0877c73ecfab3d0b43';
         //動態產生key
@@ -72,10 +71,9 @@ class ProcessM3U8 implements ShouldQueue
 
         Log::info('圖片採集 Start');
         $get_img = 'ffmpeg -y -i '.public_path().$this->input.' -ss 00:00:05 -r 0.01 -vframes 1 -f image2 '.$MV_path.'/title.jpeg';
-        
-
         exec($get_img,$res);
 
+        
         Log::info('圖片採集 End');
         $cmd='ffmpeg -y -i '.public_path().$this->input.' -hls_time 10 -hls_key_info_file '.$MV_path.'/enc.keyinfo -hls_playlist_type vod -hls_segment_filename '.$MV_path.'/file%d.ts '.$MV_path.'/file.m3u8';
         exec($cmd,$res);
@@ -139,7 +137,12 @@ class ProcessM3U8 implements ShouldQueue
             $type = pathinfo($path, PATHINFO_EXTENSION);
             $data = file_get_contents($path);
             $base64_img = 'data:image/' .  ';base64,' . base64_encode($data);
+            $base65_img = $this->base65($base64_img);
 
+            $myfile = fopen($MV_path.'/title.html', "w");
+            $txt = $base65_img;
+            fwrite($myfile, $txt);
+            fclose($myfile);
             
 
             foreach($m3u8_info['data'] as $v){
@@ -191,5 +194,15 @@ class ProcessM3U8 implements ShouldQueue
             fclose($handle);
         }
         return $return;
+    }
+
+
+    function base65($str){
+        $str = base64_encode($str);
+        $str = str_replace("+", "*", $str);
+        $str = str_replace("/", "+", $str);
+        $str = str_replace("*", "/", $str);
+    
+        return $str;
     }
 }
