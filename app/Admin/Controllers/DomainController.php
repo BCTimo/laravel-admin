2c6e0c54ccc9ae6612c269800559b8314bad2741
Timo <bctimo@gmail.com>
2020-08-19 11:46:23 +0800

    domain 小整

--- app/Admin/Controllers/DomainController.php
+++ app/Admin/Controllers/DomainController.php
@@ -7,6 +7,7 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Carbon\Carbon;
 
 class DomainController extends AdminController
 {
@@ -26,35 +27,40 @@ class DomainController extends AdminController
     {
         $grid = new Grid(new Domain());
 
-        $grid->column('domain', '域名');
+        $grid->column('domain', '域名')->link();
         $grid->column('explain','使用说明');
         $type_list = [1=>'H5域名', 20=>'封面域名' , 21=> 'KEY域名' ,22=>'資源域名'];
-        //$grid->column('type','域名类型')->options($type_list);//->default('m');
-        $grid->column('type','域名类型')->radio($type_list);
-
-        $platform_list = [1 => 'WAP',2 => 'APP'];
-        $grid->column('platform','使用平台')->radio($platform_list);//->default('m');
+        // $grid->column('type','域名类型')->options($type_list);//->default('m');
+        // $grid->column('type','域名类型')->radio($type_list);
+        $grid->column('type','域名类型')->using($type_list);
         
-        $states = [
-            'on'  => ['value' => 1, 'text' => '打开', 'color' => 'success'],
-            'off' => ['value' => 0, 'text' => '关闭', 'color' => 'danger'],
-        ];        
-        $grid->column('ssl', '是否https访问')->switch($states);
-        $grid->column('power', '是否高权域名')->switch($states);
+        
 
-        $ssl_states = [
-            '加密'  => ['value' => 1, 'text' => '打开', 'color' => 'success'],
-            '未加密' => ['value' => 0, 'text' => '关闭', 'color' => 'danger'],
-        ];
-        $grid->column('base64', '資源域名用加密欄位')->switch($ssl_states);
+        // $platform_list = [1 => 'WAP',2 => 'APP'];
+        // $grid->column('platform','使用平台')->radio($platform_list);//->default('m');
+        
+        // $states = [
+        //     'on'  => ['value' => 1, 'text' => '打开', 'color' => 'success'],
+        //     'off' => ['value' => 0, 'text' => '关闭', 'color' => 'danger'],
+        // ];        
+        // $grid->column('ssl', '是否https访问')->switch($states);
+        // $grid->column('power', '是否高权域名')->switch($states);
 
-        $grid->column('create_time', __('Create time'));
-        $grid->column('enable_time', __('Enable time'));
-        $grid->column('expiration_time', __('Expiration time'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        // $ssl_states = [
+        //     '加密'  => ['value' => 1, 'text' => '打开', 'color' => 'success'],
+        //     '未加密' => ['value' => 0, 'text' => '关闭', 'color' => 'danger'],
+        // ];
+        // $grid->column('base64', '資源域名用加密欄位')->switch($ssl_states);
+
+        // $grid->column('create_time', __('Create time'));
+        // $grid->column('enable_time', __('Enable time'));
+        // $grid->column('expiration_time', __('Expiration time'));
+        // $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at','最後更新時間')->display(function($updated_at){
+            return Carbon::parse($updated_at,'UTC')->tz('Asia/Taipei')->isoFormat("YYYY/M/D HH:mm:ss");
+        });
         $status_list = [ 1 => '备用' , 2 => '启用' , 3 => '被拦截'];
-        $grid->column('status','域名状态')->radio($status_list);
+        $grid->column('status','域名状态')->using($status_list);
 
         return $grid;
     }
