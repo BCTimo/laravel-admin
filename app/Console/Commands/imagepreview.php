1d2bc10871c639fa834bdb86848f72dca5a75b0d
Timo <bctimo@gmail.com>
2020-11-03 13:35:11 +0800

    queue作法

--- app/Console/Commands/imagepreview.php
+++ app/Console/Commands/imagepreview.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\ProcessGenImage;
 use Illuminate\Console\Command;
 use App\Models\Video;
 use App\Models\Videofiles;
@@ -67,44 +68,10 @@ class imagepreview extends Command
                 $i=$i+$gap;
             }
 
+            ProcessGenImage::dispatch($video,$ts_list);
             // dd($ts_list);
-            #取間隔ts檔案轉成jpg => 轉成html
-            $j=0;
-            foreach($ts_list as $v){
-                #組檔案
-                $m3u8 ='#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-TARGETDURATION:18
-#EXT-X-MEDIA-SEQUENCE:0
-#EXT-X-PLAYLIST-TYPE:VOD
-#EXT-X-KEY:METHOD=AES-128,URI="'.env('S3_URL').$video->key_path.'",IV='.$video->iv.'
-#EXTINF:3,
-'.env('S3_URL').$v.'
-#EXT-X-ENDLIST
-                ';
-                $video_path = '/tmp/mv/'.$video->id.'/';
-                exec('mkdir -p ' . $video_path);
-                // #寫檔案+產JPG
-                $file = fopen($video_path."tmp.m3u8","w+"); //開啟檔案
-                fwrite($file,$m3u8);
-                fclose($file);
-
-                $zeroj = str_pad($j,3,'0',STR_PAD_LEFT);
-                $cutimg_cmd = 'ffmpeg -y -allowed_extensions ALL -protocol_whitelist file,http,https,tcp,tls,crypto -i '.$video_path.'/tmp.m3u8 -ss 00:00:00.0001 -vframes 1 '.$video_path.$zeroj.'.jpg';
-                exec($cutimg_cmd,$res,$m);
-                
-                if($m ==0 ){
-                    #加密
-                    $encrypt_img = 'echo \'data:image/jpeg;base64,\' > '.$video_path.$zeroj.'.html && base64 '.$video_path.$zeroj.".jpg | sed 's/[+]/*/g' |sed 's/\//+/g' | sed 's/[*]/\//g' >> ".$video_path.$zeroj.".html";
-                    exec($encrypt_img);
-                    $j++;
-                }
-                
-                #上傳至AWS
-                $upload_s3_cmd = '/usr/local/bin/aws s3 sync '.$video_path.' s3://mv-video/mv/'.$video->id.'/  --exclude "*" --include "*.html"';
-                exec($upload_s3_cmd);
-            }
-
+            
+            
         }
 
         return true;
